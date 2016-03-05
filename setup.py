56c02d67238341fed8bde63af3824d4b416cf872
Eugene Lee <460893751@qq.com>
2016-03-04 22:09:21 -0800

    modify setup.py

--- setup.py
+++ setup.py
@@ -4,14 +4,14 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='hedera',
+    name='mininet platform',
     version='0.0.0',
-    description='Implementation of Hedera Scheduling System',
-    author='Ian Walsh, Anh Truong',
-    author_email='iwalsh, anhlt92@stanford.edu',
+    description='Implementation of Dynamic Hybrid Routing',
+    author='Eugene Lee',
+    author_email='Eugene Lee, 460893751@qq.com',
     packages=find_packages(exclude='test'),
     long_description="""\
-For Stanford CS244 Spring 2015 lab 3. Build on top of Brandon Heller's RIPL and RIPLPOX libraries
+For Dynamic Hybrid Routing, Based on Stanford CS244 Spring 2015 lab 3. Build on top of Brandon Heller's RIPL and RIPLPOX libraries
       """,
       classifiers=[
           "License :: OSI Approved :: GNU General Public License (GPL)",
